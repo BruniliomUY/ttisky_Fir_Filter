eba2ac356fc6c79f2d6c74fa78d34760ae695515
Bruno <brunomoreira.uy@gmail.com>
2026-07-17 21:39:11 -0300

    Test fix 4.2

--- src/SatTruncFP.v
+++ src/SatTruncFP.v
@@ -33,24 +33,17 @@ module SatTruncFP
 //=======================================================
 //  Structural coding
 //=======================================================
-	generate
-	   if (NBF_XI >= NBF_XO)
-	     begin : gen_trunc_wide
-		assign aux_trunc = i_data[NBF_XI-1 -: NBF_XO];
-	     end
-	   else
-	     begin : gen_trunc_narrow
-		assign	aux_trunc = {i_data[NBF_XI-1:0],{(NBF_XO - NBF_XI){1'b0}}};
-	     end
-	   if (NBI_XI > NBI_XO)
-	     begin : gen_sat_shrink
-		assign	condicion		=	(i_data[(NB_XI-2)-:(NBI_XI-NBI_XO)] == {(NBI_XI-NBI_XO){i_data[NB_XI-1]}});
-		assign	resultado1		=	{i_data[(NB_XI-1)],{(NB_XO-1){~i_data[(NB_XI-1)]}}};
-		assign	resultado2		=	{i_data[(NB_XI-1)],i_data[NBF_XI +:NBI_XO-1],aux_trunc};
-		assign	aux_Sat			=	condicion	?	resultado2	:	resultado1;
-	     end
-	   else
-	     begin : gen_sat_grow
+generate
+  // If there are no fractional output bits, skip any zero-repeat concatenations.
+  if (NBF_XO == 0) begin : gen_no_frac
+    // No aux_trunc bits. Provide direct aux_Sat construction without using aux_trunc.
+    if (NBI_XI > NBI_XO) begin : gen_sat_shrink_no_frac
+      assign condicion    = (i_data[(NB_XI-2)-:(NBI_XI-NBI_XO)] == {(NBI_XI-NBI_XO){i_data[NB_XI-1]}});
+      assign resultado1   = {i_data[(NB_XI-1)],{(NB_XO-1){~i_data[(NB_XI-1)]}}};
+      // resultado2 omits aux_trunc because NBF_XO == 0
+      assign resultado2   = {i_data[(NB_XI-1)], i_data[NBF_XI +: NBI_XO-1]};
+      assign aux_Sat      = condicion ? resultado2 : resultado1;
+    end else begin : gen_sat_grow_no_frac
 		if (NBI_XO == NBI_XI)
                   begin : gen_sat_equal
                      assign  aux_Sat = {i_data[(NB_XI-1)-:NBI_XI],aux_trunc};
